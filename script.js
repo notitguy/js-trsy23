d115ff399f743a8caf1750e7373e5a454af5d3d9
notitguy <notitguy@email.com>
2024-03-18 01:06:23 +0100

    numbers

---
+++ script.js
@@ -0,0 +1,9 @@
+const str = "developer";
+
+x = str.charAt(0).toUpperCase() + str.slice(1, 9);
+// x = str.charAt(0).toUpperCase() + str;
+
+console.log(x);
+console.log(typeof x);
+
+// https://youtu.be/BI1o2H9z9fo?t=7250
