03cb4fbc4348fb5a2391e3d8be085eb11aff7246
Notitguy Office <kelvin77@seznam.cz>
2024-03-21 17:28:46 +0100

    mouse events

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-function removeItem(itemNumber) {
-  const ul = document.querySelector("ul");
-  const li = document.querySelector(`li:nth-child(${itemNumber})`);
+const button = document.querySelector('button');
+const list = document.querySelector('ul');
+const listItems = document.querySelectorAll('li');
 
-  if (itemNumber > ul.children.length || itemNumber <= 0) {
-    console.log("Item doesn't exist");
-  } else {
-    ul.removeChild(li);
-  }
+function clearBtn() {
+  
+  const defaultListItem = document.createElement('li');
+  defaultListItem.textContent = 'Empty list';
+
+  listItems.forEach(item => {
+    item.remove();
+    list.insertAdjacentElement('afterbegin', defaultListItem);
+  });
 }
 
-removeItem(2);
-
-// Events
+button.addEventListener('click', clearBtn);
